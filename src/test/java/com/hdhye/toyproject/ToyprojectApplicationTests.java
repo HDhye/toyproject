556a6efd784e92a2d66a2b0f6b86a406bf100683
HDhye <hdhye89@gmail.com>
2023-07-19 09:42:51 +0900

    게시판 목록 조회(심플VER)

--- src/test/java/com/hdhye/toyproject/ToyprojectApplicationTests.java
+++ src/test/java/com/hdhye/toyproject/ToyprojectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ToyprojectApplicationTests {
+class ToyProjectApplicationTests {
 
     @Test
     void contextLoads() {
