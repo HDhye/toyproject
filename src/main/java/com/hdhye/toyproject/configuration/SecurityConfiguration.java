33e6a1acb0db9efcf72ebc5a2051d09aa60e5b22
HDhye <hdhye89@gmail.com>
2023-07-21 09:30:19 +0900

    시큐리티 약간 추가

--- src/main/java/com/hdhye/toyproject/configuration/SecurityConfiguration.java
+++ src/main/java/com/hdhye/toyproject/configuration/SecurityConfiguration.java
@@ -1,8 +1,29 @@
 package com.hdhye.toyproject.configuration;
 
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration      // 빈 등록
 public class SecurityConfiguration {
+
+    /* 시큐리티 설정 무시 정적 리소스 등록 */
+    @Bean
+    public WebSecurityCustomizer configure(){
+        return (web) -> web.ignoring().antMatchers("/css/**", "images/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) {
+
+        http.csrf().disable();
+        return http.authorizeRequests().build();
+
+
+    }
+
 
 
 
