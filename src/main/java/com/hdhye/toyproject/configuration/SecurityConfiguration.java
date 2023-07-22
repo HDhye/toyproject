f685d0f130f7250874e31bf5979331138dc00d58
HDhye <hdhye89@gmail.com>
2023-07-22 18:20:45 +0900

    시큐리티 설정 추가

--- src/main/java/com/hdhye/toyproject/configuration/SecurityConfiguration.java
+++ src/main/java/com/hdhye/toyproject/configuration/SecurityConfiguration.java
@@ -6,13 +6,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration      // 빈 등록
 public class SecurityConfiguration {
 
-    @Autowired
     private PrincipalOauth2UserService principalOauth2UserService;
+
+    @Autowired
+    public SecurityConfiguration(PrincipalOauth2UserService principalOauth2UserService) {
+        this.principalOauth2UserService = principalOauth2UserService;
+    }
+
+    /* 비밀번호 암호화 사용객체 등록 */
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     /* 시큐리티 설정 무시 정적 리소스 등록 */
     @Bean
@@ -24,21 +37,33 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 
-        return http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/").authenticated()
-                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+        return http.csrf().disable()    // csrt 토큰위주 공격을 막기위한 작업(default == on)
+                .authorizeRequests()// 요청에 대한 권한 체크를 어떻게 할 것인지 지정
+                .antMatchers("/").authenticated()// /로 들어온 것에 대해서 하나의 권한을 등록(인증된 사용자만 접근 가능)
+//                .antMatchers(HttpMethod.GET, "/notice/**").hasRole("MEMBER") // 멤버는 조회만 가능
+//                .antMatchers(HttpMethod.POST, "/notice/**").hasRole("ADMIN")
+//                .antMatchers("/admin/**").access("hasRole('ADMIN')")
                 .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/loginProc")
+            .and()
+                .formLogin()// 로그인 form을 따로 이용해 로그인 처리할 것이다.
+                .loginPage("/login") // 로그인페이지 submit요청 경로. (POST일 경우에 설정을 따로 안해주면 여기로 찾아온다)
+                .loginProcessingUrl("/logipROC")
+                .successForwardUrl("/")  // 성공시 페이지
                 .defaultSuccessUrl("/")
-                .and()
+            .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // 로그아웃시 요청 경로
+                .deleteCookies("JSESSIONID") // 쿠키제거
+                .invalidateHttpSession(true)                        // 세션도 무효화
+            .and()
+                .exceptionHandling() // 인가/인증 exception핸들링 설정
+                .accessDeniedPage("/denied")    // 권한없는 기능 요청시 나타날 페이지
+            .and()
                 .oauth2Login()
                 .loginPage("/login")
                 .userInfoEndpoint()
-                .userService(principalOauth2UserService).build();
+                .userService(principalOauth2UserService)
+                .build();
 
 
 
--- src/main/java/com/hdhye/toyproject/model/service/PrincipalOauth2UserService.java
+++ src/main/java/com/hdhye/toyproject/model/service/PrincipalOauth2UserService.java
@@ -1,60 +1,20 @@
 package com.hdhye.toyproject.model.service;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import com.hdhye.toyproject.model.dto.UserDTO;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Map;
-
 @Service
-public class PrincipalOauth2UserService implements UserDetails, OAuth2User {
-    /* user 타입과 oauth타입을 둘다 다루기 위해 2가지 인터페이스를 모두 구현한다. */
+public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
 
-    @Override
-    public Map<String, Object> getAttributes() {
-        return null;
-    }
+    private UserDTO user;
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
-
-    @Override
-    public String getName() {
-        return null;
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        return super.loadUser(userRequest);
     }
 }
